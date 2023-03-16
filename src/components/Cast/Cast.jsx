33f531f3f951cfc2014d5b083e3dd95f1203c444
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-03-16 13:02:50 +0200

    actors
    actors

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as API from '../../services/api';
-import { List } from './Cast.styled';
+import { List, ActorItem } from './Cast.styled';
 import defaultImgCast from '../../img/anon.jpg';
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const GOOGLE_SERACH = 'https://www.google.com/search?q=';
 
 function Cast() {
   const [cast, setCast] = useState([]);
@@ -23,18 +24,24 @@ function Cast() {
     <div>
       <List>
         {cast.map(cast => (
-          <li key={cast.cast_id}>
-            <img
-              width={250}
-              src={
-                cast.profile_path
-                  ? BASE_IMG_URL + cast.profile_path
-                  : defaultImgCast
-              }
-              alt={cast.original_name}
-            />
-            <p>{cast.original_name}</p>
-          </li>
+          <ActorItem key={cast.cast_id}>
+            <a
+              href={GOOGLE_SERACH + cast.original_name}
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <img
+                width={250}
+                src={
+                  cast.profile_path
+                    ? BASE_IMG_URL + cast.profile_path
+                    : defaultImgCast
+                }
+                alt={cast.original_name}
+              />
+              <p>{cast.original_name}</p>
+            </a>
+          </ActorItem>
         ))}
       </List>
     </div>
--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -11,3 +11,11 @@ export const List = styled.ul`
   gap: 15px;
   border-bottom: 1px solid black;
 `;
+export const ActorItem = styled.li`
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    box-shadow: 2px 2px 6px 2px rgba(254, 60, 1, 0.9);
+    transform: scale(1.05);
+  }
+`;
